b420712cf9dfa8686eb4c5fdbd197084b8080f9e
Mateusz Glowa <glowa.mateusz@gmail.com>
2025-01-25 18:26:54 +0100

    change the trigger to every 5 hours

--- function_app.py
+++ function_app.py
@@ -14,7 +14,7 @@ from sendgrid.helpers.mail import Mail
 
 app = func.FunctionApp()
 
-@app.timer_trigger(schedule="0 0 18 * * *", arg_name="myTimer", run_on_startup=False,
+@app.timer_trigger(schedule="0 0 */5 * * *", arg_name="myTimer", run_on_startup=False,
               use_monitor=False) 
 def func_timer_trigger(myTimer: func.TimerRequest) -> None:
     if myTimer.past_due:
